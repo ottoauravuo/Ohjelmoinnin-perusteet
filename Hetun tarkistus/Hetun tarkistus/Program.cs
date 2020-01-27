c2db62b9a5fcaa29c16742a3fd064d58143b3cf3
Otto Auravuo <otto.auravuo@hotmail.com>
2020-01-27 21:59:05 +0200

    Lisätty tehtävä

---
+++ Hetun tarkistus/Hetun tarkistus/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+
+namespace Hetun_tarkistus
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            do
+            {
+                string hetuNumber = Intro();
+                if (DateChecker(hetuNumber))
+                {
+                    string check = LastMark(hetuNumber);
+                    bool isReal = LastChar(check, hetuNumber.Substring(10));
+                    PrintData(isReal);
+                    break;
+                }
+            } while (true);
+        }
+        static string Intro()
+        {
+            Console.Write("Syötä henkilötunnus: ");
+            return Console.ReadLine();
+        }
+        static string LastMark(string hetuNumber)
+        {
+            string checkMark = "0123456789ABCDEFHJKLMNPRSTUVWXY";
+            string strHetu = hetuNumber.Remove(6, 1);
+            int hetuCheck = int.Parse(strHetu.Remove(9));
+            hetuCheck = hetuCheck % 31;
+            string lastChar = checkMark[hetuCheck].ToString();
+            return lastChar;
+        }
+        static bool DateChecker(string Number)
+        {
+            int day = int.Parse(Number.Substring(0, 2));
+            int month = int.Parse(Number.Substring(2, 2));
+            int year = int.Parse(Number.Substring(4, 2));
+
+            try
+            {
+                DateTime d = new DateTime(year, month, day);
+            }
+            catch
+            {
+                Console.WriteLine("Virheellinen päivämäärä");
+                return false;
+            }
+            return true;
+        }
+        static bool LastChar(string letter1, string letter2)
+        {
+            if (letter2 == letter1)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        static void PrintData(bool data)
+        {
+            if (data)
+            {
+                Console.WriteLine("Henkilötunnus on oikea");
+            }
+            else
+            {
+                Console.WriteLine("Henkilötunnus on väärä");
+            }
+        }
+    }
+}
